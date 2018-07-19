ed018abadfa1bd45516f07275723cf8623d88a74
NIthin Antony <nithinparathara007@gmail.com>
2018-07-19 23:10:05 +0530

    Update server.js

--- src/main/resources/static/server.js
+++ src/main/resources/static/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/busmatesweb'));
+app.use(express.static(__dirname + '/src/main/resources/static/dist/busmatesweb'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/busmatesweb/index.html'));
+res.sendFile(path.join(__dirname+'/src/main/resources/static/dist/busmatesweb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
